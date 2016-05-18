1d126d5e9448e61934ad64b7ec980204944edf35
Taylor Burgess <tayloraburgess@gmail.com>
2016-05-18 17:46:43 -0400

    Add support for emphasis

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ var fs = require("fs");
 var http = require("http");
 var program = require("commander");
 
+String.prototype.insert = function(startIndex, endIndex, subString) {
+
+	var beforeString = this.slice(0, startIndex);
+	var afterString = this.slice(endIndex + 1, this.length);
+	return beforeString + subString + afterString;
+}
+
+String.prototype.replaceChar = function(replaceIndex, replaceCharacter) {
+
+	var beforeString = this.slice(0, replaceIndex);
+	var afterString = this.slice(replaceIndex + 1, this.length);
+	return beforeString + replaceCharacter + afterString;
+}
+
 var generatePreview = (file) => {
 
 	var server = http.createServer( (request, response) => {
@@ -13,16 +27,31 @@ var generatePreview = (file) => {
 			if (err) throw err;
 
 			var fileText = data.toString();
-			var textLines = fileText.split("\n");
+			var htmlText= "";
 
-			for (i = 0; i < textLines.length; i++) {
-				textLines[i] += "<br>";
+			var boldCount = 0;
+			for (i = 0; i < fileText.length; i++) {
+				if (fileText.charAt(i) ==  "*" | fileText.charAt(i) ==  "_") {
+
+					if (boldCount) {
+						htmlText+= "</em>";
+						boldCount = 0;
+					}
+					else {
+						htmlText += "<em>";
+						boldCount = 1;
+					}
+
+				}
+
+				else if (fileText.charAt(i) == "\n") htmlText += "<br>";
+
+				else htmlText += fileText.charAt(i);
+
 			}
 
-			var htmlDisplay = textLines.join("");
-
 			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.end(htmlDisplay);
+			response.end(htmlText);
 
 		});
 
