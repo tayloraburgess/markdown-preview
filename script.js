bf8c6c8c2cbfb089dfd59a104378b7fd58bf7509
Taylor Burgess <tayloraburgess@gmail.com>
2016-05-19 14:17:33 -0400

    Refactor main loop
    Move algorithm that analyses individual characters for emphasis out of main loop and into a separate function. Cleans up code and will make it much more scalable/expandable as mroe interpretative features are added--would be a huge mess if all analysis was done directly within the main loop.

--- script.js
+++ script.js
@@ -4,18 +4,67 @@ var fs = require("fs");
 var http = require("http");
 var program = require("commander");
 
-String.prototype.insert = function(startIndex, endIndex, subString) {
+var boldCount = 0;
+var strongCount = 0;
 
+String.prototype.insert = function(startIndex, endIndex, subString) {
 	var beforeString = this.slice(0, startIndex);
 	var afterString = this.slice(endIndex + 1, this.length);
 	return beforeString + subString + afterString;
 }
 
 String.prototype.replaceChar = function(replaceIndex, replaceCharacter) {
-
 	var beforeString = this.slice(0, replaceIndex);
 	var afterString = this.slice(replaceIndex + 1, this.length);
 	return beforeString + replaceCharacter + afterString;
+}
+
+function emphasis (inputText, checkChar, charIndex) {
+
+	var returnHtml = "";
+
+	if (inputText.charAt(charIndex - 1) != checkChar && inputText.charAt(charIndex + 1) != checkChar) {
+
+		if (boldCount) {
+			returnHtml+= "</em>";
+			boldCount = 0;
+		}
+
+		else {
+			returnHtml += "<em>";
+			boldCount = 1;
+		}
+	}
+	else if (inputText.charAt(charIndex - 1) == checkChar) {
+
+		if (inputText.charAt(charIndex - 2) == checkChar) {
+
+			if (strongCount && boldCount) {
+				returnHtml+= "</strong></em>";
+				strongCount = 0;
+				boldCount = 0;
+			}
+
+			else {
+				returnHtml += "<strong><em>";
+				strongCount = 1;
+				boldCount = 1;
+			}
+		}
+		else if (inputText.charAt(charIndex + 1) != checkChar) {
+
+			if (strongCount) {
+				returnHtml+= "</strong>";
+				strongCount = 0;
+			}
+
+			else {
+				returnHtml += "<strong>";
+				strongCount = 1;
+			}
+		}
+	}
+	return returnHtml;
 }
 
 var generatePreview = (file) => {
@@ -29,71 +78,24 @@ var generatePreview = (file) => {
 			var fileText = data.toString();
 			var htmlText= "";
 
-			var boldCount = 0;
-			var strongCount = 0;
-
 			for (i = 0; i < fileText.length; i++) {
 
 				var thisChar = fileText.charAt(i);
 
 				if (thisChar != "\\") {
 
-					if (thisChar ==  "*" | thisChar == "_") {
-
-						if (fileText.charAt(i - 1) != thisChar && fileText.charAt(i + 1) != thisChar) {
-
-							if (boldCount) {
-								htmlText+= "</em>";
-								boldCount = 0;
-							}
-							else {
-								htmlText += "<em>";
-								boldCount = 1;
-							}
-						}
-						else if (fileText.charAt(i - 1) == thisChar) {
-
-							if (fileText.charAt(i - 2) == thisChar) {
-
-								if (strongCount && boldCount) {
-									htmlText+= "</strong></em>";
-									strongCount = 0;
-									boldCount = 0;
-								}
-								else {
-									htmlText += "<strong><em>";
-									strongCount = 1;
-									boldCount = 1;
-								}
-
-							}
-
-							else if (fileText.charAt(i + 1) != thisChar) {
-
-								if (strongCount) {
-									htmlText+= "</strong>";
-									strongCount = 0;
-								}
-								else {
-									htmlText += "<strong>";
-									strongCount = 1;
-								}
-							}
-						}
-					}
-
+					if (thisChar ==  "*" | thisChar == "_") htmlText += emphasis(fileText, thisChar, i);
 					else if (fileText.charAt(i) == "\n") htmlText += "<br>";
-
 					else htmlText += fileText.charAt(i);
 
 				}
 
 				else {
+
 					i++;
 					htmlText += fileText.charAt(i);
 
 				}
-
 			}
 
 			response.writeHead(200, {'Content-Type': 'text/html'});
@@ -108,6 +110,6 @@ var generatePreview = (file) => {
 program
 	.version("1.0.0")
 	.usage("<file...>")
-	.action(generatePreview)
+	.action(generatePreview);
 
-program.parse(process.argv)
+program.parse(process.argv);
